27b1c36ac9c6fb6f942446f1c9a3dd88979a983e
Avinash209 <avinash.richin20@gmail.com>
2024-04-09 23:59:21 +0530

    Connected to db

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path : './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({path: './env'})
+
+
+
+connectDB()
+
+
+
+
+// function connectDB(){
+
+// }
+
+/*
+
+( async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       app.on("error",()=>{
+        console.log("Error", error);
+        throw error
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on PORT ${process.env.PORT}`)
+       })
+    } catch (error) {
+        console.log("Error : ", error)
+        throw error
+    }
+})()
+
+*/
