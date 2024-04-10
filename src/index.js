e6135b3d8865f94d14bcdcf2b76afb5e915bf57f
Avinash209 <avinash.richin20@gmail.com>
2024-04-10 12:37:46 +0530

    Added user and video models

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ dotenv.config({path: './env'})
 
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running on PORT: ${process.env.P}`);
+    })
+})
+.catch((err) => {
+    console.log('MONGODB connection failed !!!',err);
+})
 
 
 
